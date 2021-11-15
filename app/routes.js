57cb59645dbc23952bc398e72fd3d58b474f1243
rachelderbyshire <84026497+rachelderbyshire@users.noreply.github.com>
2021-11-15 09:41:04 +0000

    radio redirect package

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
 
 // Add your routes here - above the module.exports line
 
