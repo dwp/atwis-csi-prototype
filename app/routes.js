7a0f3257a3d0157178c6255974bca7f6ce6f858d
rachelderbyshire <84026497+rachelderbyshire@users.noreply.github.com>
2022-03-02 09:31:43 +0000

    archive

--- app/routes.js
+++ app/routes.js
@@ -294,6 +294,123 @@ router.post('/current/cases/searchlight/benefits/', function(req, res, next){ 
 return res.redirect("/current/cases/claim#your-tasks"); next();
 })
 
+//live-8
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "Incapacity Benefit, including National Insurance credits only" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Employment and Support Allowance, including National Insurance credits only" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Severe Disablement Allowance" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Income Support for illness or disability" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Personal Independence Payment" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "Employment and Support Allowance, including National Insurance credits only" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Severe Disablement Allowance" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Income Support for illness or disability" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+      case "Personal Independence Payment" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "Severe Disablement Allowance" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+   case "Income Support for illness or disability" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+      case "Personal Independence Payment" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "Income Support for illness or disability" :  return res.redirect("/current/cases/searchlight/benefits/exceptions"); next(); break;
+      case "Personal Independence Payment" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "Personal Independence Payment" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+router.post('/live-8/cases/searchlight/benefits/check', function(req, res, next){ 
+
+ if (req.session.data['benefits']){
+  for (i = 0; i<req.session.data['benefits'].length; i++){
+  switch (req.session.data['benefits'][i]){
+
+   case "none" :  return res.redirect("/live-8/cases/claim#your-tasks"); next(); break;
+   default: continue;
+   }
+   break;
+  }
+ } else {
+
+ }
+return res.redirect("/live-8/cases/claim#your-tasks"); next();
+})
+
 //live-6
 
 router.post('/live-6/cases/searchlight/benefits/check', function(req, res, next){ 
