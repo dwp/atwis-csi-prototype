493f81f4099eaef3106ff718d4d1858e6eec2d4d
rachelderbyshire <84026497+rachelderbyshire@users.noreply.github.com>
2022-10-31 10:55:08 +0000

    Dereck smith case

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -28,11 +28,7 @@ module.exports = {
 'notificationthomas' : "",
 'wpa-report' : "",
 
-'Searchligh' : "Yes and application details match",
-'found-nino' : "Yes",
-'national-insurance-number' : "AB 123456 C",
-'appointee' : "No",
-'benefits' : "No",
+
 'job-deatils-contact' : "They gave permission (apply)",
 //delete later
 
